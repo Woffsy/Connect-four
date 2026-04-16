9e65b890bf3dbb1431f6a5c0bd2231c8b1c5b181
sigurd <sigurdskudal@gmail.com>
2026-04-16 12:11:12 +0200

    fikset noe sjekkSeier

--- bot.py
+++ bot.py
@@ -32,9 +32,9 @@ class Bot:
         return False
             
         
-    def sjekkSeierTrekk(self,brett,kol,spiller):
-        for rute in brett.brett[kol]:
-            if r.farge == HOVER_FARGE or r.farge == WHITE:
+    def sjekkSeierTrekk(self,kol:list[Rute],spiller:str):
+        for rute in kol:
+            if rute.farge == HOVER_FARGE or rute.farge == WHITE:
                 for r in RETTNINGER:
                     antPåRad=0
                     for s in range(-3,4):
@@ -47,21 +47,8 @@ class Bot:
                                 return True
                 return False       
     
-
-    def sjekkTapTrekk(self,brett,kol,spiller):
-        for rute in brett.brett[kol]:
-            if r.farge == HOVER_FARGE or r.farge == WHITE:
-                for r in RETTNINGER:
-                    antPåRad=0
-                    for s in range(-3,4):
-                        if rute.kol+s*r[0] in range(0,7) and rute.rad+s*r[1] in range(0,6):
-                            if s == 0:
-                                antPåRad+=1
-                            else:
-                                antPåRad=antPåRad+1 if self.brett[rute.kol+s*r[0]][rute.rad+s*r[1]].farge == SPILLER_FARGER[spiller].farge else 0
-                            if antPåRad>=4:
-                                return True
-                return False
+    def sjekkTapTrekk(self):
+        pass
                         
 
                         
