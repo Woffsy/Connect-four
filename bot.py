5f71cbab6b75373153e63753237865d1584672c3
sigurd <sigurdskudal@gmail.com>
2026-04-16 11:24:44 +0200

    halveis i sjekktaptrekk fikset ikke fungerende kode

--- bot.py
+++ bot.py
@@ -50,7 +50,20 @@ class Bot:
                                 return True
                 return False       
     
-    def sjekkTapendeTrekk(self)
+    def sjekkTapTrekk(self,brett,kol,spiller):
+        for rute in brett.brett[kol]:
+            if r.farge == HOVER_FARGE or r.farge == WHITE:
+                for r in RETTNINGER:
+                    antPåRad=0
+                    for s in range(-3,4):
+                        if rute.kol+s*r[0] in range(0,7) and rute.rad+s*r[1] in range(0,6):
+                            if s == 0:
+                                antPåRad+=1
+                            else:
+                                antPåRad=antPåRad+1 if self.brett[rute.kol+s*r[0]][rute.rad+s*r[1]].farge == SPILLER_FARGER[spiller].farge else 0
+                            if antPåRad>=4:
+                                return True
+                return False
                         
 
                         
